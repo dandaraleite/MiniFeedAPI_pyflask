8927368993ee216804680468e677a371a8f8c802
Dandara <dandaraleite2@gmail.com>
2023-08-22 21:31:42 -0300

    add security scheme in api

--- factory.py
+++ factory.py
@@ -1,15 +1,28 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from spectree import SpecTree
-from flask_jwt_extended import JWTManager
+from spectree import SecurityScheme, SpecTree
+
 
 from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
 jwt = JWTManager()
-api = SpecTree("flask", title="Mini Feed API", version="v.1.0", path="docs")
+api = SpecTree(
+    "flask",
+    title="Mini Feed API",
+    version="v.1.0",
+    path="docs",
+    security_schemes=[
+        SecurityScheme(
+            name="api_key",
+            data={"type": "apiKey", "name": "Authorization", "in": "header"},
+        )
+    ],
+    security={"api_key": []},
+)
 
 def create_app():
     app = Flask(__name__)
@@ -20,13 +33,21 @@ def create_app():
     
     db.init_app(app)
 
-    from models import User
+    from models import User, Post
+    
     migrate.init_app(app, db)
 
-    from controllers import user_controller, auth_controller
+    @jwt.user_lookup_loader
+    def user_load(header, data):
+        current_user = User.query.filter_by(username=data["sub"]).first()
+
+        return current_user
+
+    from controllers import user_controller, auth_controller, posts_controller
 
     app.register_blueprint(user_controller)
     app.register_blueprint(auth_controller)
+    app.register_blueprint(posts_controller)
 
     api.register(app)
 
